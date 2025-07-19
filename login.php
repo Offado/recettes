abd0fd969207566189f62c6dfa97838f4e39a234
offolome <offolomebillchrisjonathanadon@gmail.com>
2025-07-19 15:26:33 +0000

    cours_php

--- login.php
+++ login.php
@@ -1,13 +1,12 @@
 <!--
-···Si utilisateur/trice est non identifié(e), on affiche le formulaire
+   Si utilisateur/trice est non identifié(e), on affiche le formulaire
 -->
-
 <?php if (!isset($_SESSION['LOGGED_USER'])) : ?>
     <form action="submit_login.php" method="POST">
         <!-- si message d'erreur on l'affiche -->
         <?php if (isset($_SESSION['LOGIN_ERROR_MESSAGE'])) : ?>
             <div class="alert alert-danger" role="alert">
-            <?php echo $_SESSION['LOGIN_ERROR_MESSAGE'];
+                <?php echo $_SESSION['LOGIN_ERROR_MESSAGE'];
                 unset($_SESSION['LOGIN_ERROR_MESSAGE']); ?>
             </div>
         <?php endif; ?>
@@ -22,8 +21,7 @@
         </div>
         <button type="submit" class="btn btn-primary">Envoyer</button>
     </form>
-
-<!-- Si utilisateur/trice bien connectée on affiche un message de succès -->
+    <!-- Si utilisateur/trice bien connectée on affiche un message de succès -->
 <?php else : ?>
     <div class="alert alert-success" role="alert">
         Bonjour <?php echo $_SESSION['LOGGED_USER']['email']; ?> et bienvenue sur le site !
--- submit_login.php
+++ submit_login.php
@@ -1,20 +1,19 @@
-<?php 
+<?php
+
 session_start();
 require_once(__DIR__ . '/config/mysql.php');
 require_once(__DIR__ . '/dataconnect.php');
-require_once(__DIR__ . '/functions.php');
 require_once(__DIR__ . '/variables.php');
-
+require_once(__DIR__ . '/functions.php');
 
 /**
  * On ne traite pas les super globales provenant de l'utilisateur directement,
  * ces données doivent être testées et vérifiées.
-*/
-
+ */
 $postData = $_POST;
 
-// Validation de formulaire
-if (isset($postData['email']) && isset($postData['password'])) {
+// Validation du formulaire
+if (isset($postData['email']) &&  isset($postData['password'])) {
     if (!filter_var($postData['email'], FILTER_VALIDATE_EMAIL)) {
         $_SESSION['LOGIN_ERROR_MESSAGE'] = 'Il faut un email valide pour soumettre le formulaire.';
     } else {
@@ -25,14 +24,14 @@ if (isset($postData['email']) && isset($postData['password'])) {
             ) {
                 $_SESSION['LOGGED_USER'] = [
                     'email' => $user['email'],
-                    'id' => $user['user_id']
+                    'user_id' => $user['user_id'],
                 ];
             }
         }
 
         if (!isset($_SESSION['LOGGED_USER'])) {
-            $_SESSION['LOGGED_ERROR_MESSAGE'] = sprintf(
-                'Les informations envoyées ne permettent pas de vous identifier : (%s %s)',
+            $_SESSION['LOGIN_ERROR_MESSAGE'] = sprintf(
+                'Les informations envoyées ne permettent pas de vous identifier : (%s/%s)',
                 $postData['email'],
                 strip_tags($postData['password'])
             );
@@ -41,5 +40,3 @@ if (isset($postData['email']) && isset($postData['password'])) {
 
     redirectToUrl('index.php');
 }
-
-?>
